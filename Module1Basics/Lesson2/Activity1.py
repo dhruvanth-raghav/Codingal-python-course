ce87d2f12ac527ce4bada46edce74ffb83675be4
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-06-25 08:23:59 +0530

    finished m1 l2 a1

---
+++ Module1Basics/Lesson2/Activity1.py
@@ -0,0 +1,16 @@
+#Printing out Sentences/Characters
+print("Welcome to the world of programming")
+#Printing out Numbers
+print(1234)
+print("1234")#String
+
+print("This is the first line\n")
+#print()
+print("This is the second line")
+
+#printing multiple things
+print("Hello",15)
+print("My name is","dhruvanth")
+
+print("Happy",end="***")
+print("Birthday")
