9ee81d0c2d5416290d4fefcea7ba79cf45dacc12
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-06-25 10:30:01 +0530

    finished m1 l2 p1

---
+++ Module1Basics/Lesson2/Project.py
@@ -0,0 +1,20 @@
+Name_1="Dhruvanth Raghav"
+Birthday_1="16/03/2012"
+
+Name_2="Sanaathani"
+Birthday_2="25/04/2014"
+
+Name_3="Albert Einstein"
+Birthday_3="14/03/1879"
+
+Name_4="Cristiano Ronaldo"
+Birthday_4="05/02/1985"
+
+Name_5="Michael Jackson"
+Birthday_5="29/08/1958"
+
+print(Name_1 , Birthday_1)
+print(Name_2 , Birthday_2)
+print(Name_3 , Birthday_3)
+print(Name_4 , Birthday_4)
+print(Name_5 , Birthday_5)
