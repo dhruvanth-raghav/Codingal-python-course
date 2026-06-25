e987e319c5654c99f053776a3523d0154e76c428
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-06-25 08:42:19 +0530

    finished m1 l2 a2 & a3

---
+++ Module1Basics/Lesson2/Activity2.py
@@ -0,0 +1,10 @@
+age1=14
+print(age1)
+
+football_team="Real Madrid"
+print("My football team is",football_team)
+
+print(f"My footballtram is {football_team}")
+
+num=input("enter any number:")
+print(num)
