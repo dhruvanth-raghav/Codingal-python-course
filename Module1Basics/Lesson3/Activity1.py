9d9e6fb3cee13b8fc57086704a34355979f4d307
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-06-29 08:25:23 +0530

    finished m1 l3 a1

---
+++ Module1Basics/Lesson3/Activity1.py
@@ -0,0 +1,18 @@
+# M1 L3 A1
+
+# 1) Store an integer value in `a`.
+a=120
+# 2) Print the datatype of `a` using `type()`.
+print("datatype of a=", type(a))
+# 3) Store a decimal (float) value in `b`.
+b=13.66
+# 4) Print the datatype of `b` using `type()`.
+print("datatype of b=", type(b))
+# 5) Store a text (string) value in `c`.
+c="dhruvanth"
+# 6) Print the datatype of `c` using `type()`.
+print("datatype of c=", type(c))
+# 7) Store a boolean value (True/False) in `d`.
+d=False
+# 8) Print the datatype of `d` using `type()`.
+print("datatype of d=", type(d))
