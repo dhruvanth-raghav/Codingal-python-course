d12f0396928704f50a9abedec1caecfa13c92ba8
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-06-29 08:38:30 +0530

    finished m1 l3 a2

---
+++ Module1Basics/Lesson3/Activity2.py
@@ -0,0 +1,14 @@
+# M1 L3 A2
+age=34
+weight=789.09
+# 5) Convert `age` from an integer to a string and store it back in `age`.
+age=str(age)
+# 6) Print `age` and print its datatype again to confirm it changed.
+print("the value of age =",age,type(age))
+# 7) Convert `weight` from a float to an integer and store it back in `weight`.
+weight=int(weight)
+# 8) Print `weight` and print its datatype again to confirm it changed.
+print("the value of weight =",weight,type(weight))
+val=0
+val=bool(val)
+print("the value of val=",val, type(val))
