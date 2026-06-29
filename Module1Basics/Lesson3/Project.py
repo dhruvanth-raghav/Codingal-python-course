e7ce311e4743d171a4308898cd3cbd8c73c8be29
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-06-29 17:42:09 +0530

    finished m1 l3 p1

---
+++ Module1Basics/Lesson3/Project.py
@@ -0,0 +1,15 @@
+#Converting user's congratulation message to UPPER CASE
+message=input("Enter a congratulation message:")
+print(message.upper())
+
+#Converting "text" to "TEXT"(upper case)
+print()
+text="congratulations"
+print("Upper Case:",text.upper())
+
+#Using "upper()" with user's input
+print()
+name=input("Enter your name:")
+print("CONGRATULATIONS", name.upper()
+      ) 
+
