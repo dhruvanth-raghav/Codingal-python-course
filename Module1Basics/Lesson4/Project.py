abb49882003066922e62ff5645a86c4446d4074d
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-06-30 21:16:47 +0530

    finished m1 l4 p1

---
+++ Module1Basics/Lesson4/Project.py
@@ -0,0 +1,6 @@
+#take input from user
+num=int(input("Enter a number to find the sqaure root:"))
+#calculate the square root (using **)
+square_root=num ** 0.5
+#print the result
+print("Square root = ", square_root)
