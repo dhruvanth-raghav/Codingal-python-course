03084a6b19bff7c366c9f6811fc667760bcad52b
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-07-07 09:04:33 +0530

    finished m1 l7 a1

---
+++ Module1Basics/Lesson7/Activity1.py
@@ -0,0 +1,21 @@
+#Comparing integers
+x=1
+y=1
+
+print("x==y:",x==y)
+print("x is y",x is y)
+
+#comparing strings
+x="Ronaldo"
+y="Ronaldo"
+
+print("x==y:",x==y)
+print("x is y:",x is y)
+
+#Comparing lists
+ur_friends=["jack","jim"]
+my_friends=["jack","jim"]
+
+print("checking equality:",ur_friends==my_friends)
+print("checking identity",ur_friends is my_friends)
+
---
+++ Module1Basics/Lesson7/Project.py
@@ -0,0 +1,3 @@
+char=" "
+print(ord(char))
+
