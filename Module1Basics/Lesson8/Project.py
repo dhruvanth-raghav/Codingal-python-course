8319ea4baab1cff3b2cd04d017981ef45a458d28
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-07-13 21:46:00 +0530

    finished m1 l8 p1

--- Module1Basics/Lesson8/Project.py
+++ Module1Basics/Lesson8/Project.py
@@ -1,9 +1,50 @@
 #find two ways of swaping variables
+# 1st method
 a=2
 b=9
-print(f"a={a} b={b}")
+c=34
+print(f"a={a} b={b} c={c}")
+a , b , c= b , c ,a
+print(f"a={a} b={b} c={c}")
 
+#1st method(but with user input)
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number"))
+
+a , b , c = b , c , a
+print("~After swapping~")
+print("a=",a)
+print("b=",b)
+print("c=",c)
+
+#2nd method
+a=2
+b=5
+c=78
+
+#(d is a temp value)
+d=a 
 a=b
-b=a
+b=c
+c=d
+print("~After swapping~")
+print("a=",a)
+print("b=",b)
+print("c=",c)
 
-print(f"a={a} b={b}")
+#2nd method(but with user input)
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number"))
+
+#(d is a temp value)
+d=a 
+a=b
+b=c
+c=d
+print("~After swapping~")
+print("a=",a)
+print("b=",b)
+print("c=",c)
+
