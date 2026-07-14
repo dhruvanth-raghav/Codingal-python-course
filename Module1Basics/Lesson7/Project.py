33fc9a7362eb6fa9813f63613cbe687b7216f107
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-07-14 13:32:57 +0530

    modified m1 l7 p1

--- Module1Basics/Lesson7/Project.py
+++ Module1Basics/Lesson7/Project.py
@@ -4,13 +4,13 @@ char=input("Enter a character:")
 print("The ASCI value of",char,"is",ord(char))
 
 ch=input("Enter a character to check it's type:")
-if ch in range(48,58):
+if ord(ch) in range(48,58):
     print("Digits")
-elif ch in range(65,91):
+elif ord(ch) in range(65,91):
     print("Upper case")
-elif ch in range(97,123):
+elif ord(ch) in range(97,123):
     print("Lower case")
-elif ch==32:
+elif ord(ch)==32:
     print("Space")
 else:
     print("Special character")
