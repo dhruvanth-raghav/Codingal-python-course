7913dc21c226f279bf3a7f25247d2f6297579353
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-08-03 21:20:28 +0530

    finished m2 l5 t1-p1

---
+++ Module2Loops/Test1/Project.py
@@ -0,0 +1,11 @@
+import turtle 
+pen=turtle.Turtle()
+
+pen.speed(3)
+
+for i in range(4):
+    pen.forward(100)
+    pen.right(90)
+
+
+turtle.done()
---
+++ Module3Functions/Lesson2/Project.py
@@ -0,0 +1,3 @@
+# Define function to calculate cube
+
+# Define a function which will execute cube function if the user entered number is divisible by 3, otherwise return False
