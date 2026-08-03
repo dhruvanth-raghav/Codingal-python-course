2fed0363bb069df8ea59435e76e84bce2dda07d0
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-08-03 22:27:58 +0530

    finished m3 l2 p1

--- Module3Functions/Lesson2/Project.py
+++ Module3Functions/Lesson2/Project.py
@@ -1,3 +1,13 @@
 # Define function to calculate cube
-
+def cube(num):
+    return num*num*num
 # Define a function which will execute cube function if the user entered number is divisible by 3, otherwise return False
+def TorF(num):
+    if num%3==0:
+        return cube(num)
+    else:
+        return False
+
+a=int(input("Enter a number:"))
+ans=TorF(a)
+print(ans)
