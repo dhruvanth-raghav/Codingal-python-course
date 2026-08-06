962b7bb13b80bcbff906c3f7a98ae9fa1abeb8e6
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-08-06 19:31:02 +0530

    finished m3 l5 a3

---
+++ Module3Functions/Lesson5/Activity3.py
@@ -0,0 +1,16 @@
+import math
+
+num=36
+print(math.sqrt(num))
+
+x=-36
+print(math.fabs(x))
+
+y=5.5
+print(math.ceil(y))#rounds up
+print(math.floor(y))#round down
+
+print(math.gcd(4, 6))#(HCF)
+print(math.lcm(4,6))#(LCM)
+
+print(math.copysign(-10,15))
