8177cc2c2fd8f0ab803c58ee78f2c13f544fd6cf
Dhruvanth Raghav <dhruvanthraghav7@gmail.com>
2026-08-26 19:59:42 +0530

    finished m5 l1 p1

---
+++ Module5 OOP/Project.py
@@ -0,0 +1,35 @@
+print("===😈SUPER VILLAIN PROFILE BUILDER🦹‍♀️===")
+class Supervillain:
+    medium = "supervillain"
+
+    def __init__(self, name, power, evil_plan, weakness):
+        self.name = name
+        self.power = power
+        self.evil_plan = evil_plan
+        self.weakness = weakness
+
+
+villain1 = Supervillain("Name:Abhishek Karmakar", "Power:Master of algorithms and programming", "Evil plan:Force students to master impossible coding challenges and recruit them into his coding army", "Weaknes:Students who become better coders than him")
+villain2 = Supervillain("Name:Volt", "Power:Controls electricity", "Evil Plan:Shut down the whole city", "Weakness:Water")
+villain3 = Supervillain("Name:Mallesh", "Power:Mind control through speeches and the ability to manipulate reality with his promises", "Evil Plan:Take over the government and recruit everyone into his political army", "Jy0thini[wife]")
+
+print("===PROPERTIES OF VILLAIN 1===")
+print(villain1.name)
+print(villain1.power)
+print(villain1.evil_plan)
+print(villain1.weakness)
+print()
+
+print("===PROPERTIES OF VILLAIN 2===")
+print(villain2.name)
+print(villain2.power)
+print(villain2.evil_plan)
+print(villain2.weakness)
+print()
+
+print("===PROPERTIES OF VILLAIN 3===")
+print(villain3.name)
+print(villain3.power)
+print(villain3.evil_plan)
+print(villain3.weakness)
+print()
